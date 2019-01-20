d67a0e0db4957cc7e3688a23b40d6f14a29b7567
Rustam_Samigullin <rstsmg1@gmail.com>
2019-01-19 21:08:11 -0500

    bubble sort challenge completed

---
+++ HackerRankPractice/bubbleSort.js
@@ -0,0 +1,19 @@
+function countSwaps(a) {
+    let counter = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a.length - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                counter++
+            }
+        }
+
+    }
+    console.log('Array is sorted in ' + counter + ' swaps.');
+    console.log('First Element: ' +a[0]);
+    console.log('Last Element: ' +a[a.length-1]);
+    return counter;
+}
